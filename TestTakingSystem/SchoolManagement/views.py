341032f30ee317970ae16722c1f229a33cfb383d
Anshul Sharma <62079646+sharm192@users.noreply.github.com>
2021-02-13 20:33:47 -0600

    base theme starting 13th Feb

--- TestTakingSystem/SchoolManagement/views.py
+++ TestTakingSystem/SchoolManagement/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 
 # Home page for the complete Website 
 def home(request):
-    return render('<p>This is the home page test.</p>')
+    return render(request,'main\\base.html',{})
 # TODO 2: Code for login
 def login(request, *args):
     return HttpResponse('<p>This is the login page test.</p>')
