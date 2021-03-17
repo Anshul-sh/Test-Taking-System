453c19a19f58bc8b439d2117ed03f65fdb4b4da1
Asmita <bhartia@uwindsor.ca>
2021-03-16 20:16:14 -0400

    fixed login issue

--- TestTakingSystem/SchoolManagement/views.py
+++ TestTakingSystem/SchoolManagement/views.py
@@ -8,7 +8,7 @@ from rest_framework.views import APIView
 from SchoolManagement.models import Student,UserManager
 import os
 from django.urls import path, include
-import face_recognition
+# import face_recognition
 import cv2 
 #from SchoolManagement.serializer import ImageSerializer
 from rest_framework.response import Response
@@ -19,50 +19,50 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 
 def home(request):
-    return render(request,'main\\base.html',{})
+    return render(request,'main/base.html',{})
 
 
 # class registration_view(CreateView):
     
-class faceid(APIView):
-    #serializer_class = ImageSerializer
-    #permission_classes = [AllowAny]
-    def post(self, request):
-        loc = '\\images\\stored\\test2.jpg'
-        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        MEDIA_ROOT =os.path.join(BASE_DIR,'SchoolManagement')
-        uri  = request.POST.__getitem__('image')
-        with url_request.urlopen(uri) as resp:
-            with open((str(MEDIA_ROOT)+'\\images\\image.jpg'), 'wb') as f:
-                f.write(resp.file.read())
-        img = cv2.imread((str(MEDIA_ROOT)+'\\images\\image.jpg'),1)
+# class faceid(APIView):
+#     #serializer_class = ImageSerializer
+#     #permission_classes = [AllowAny]
+#     def post(self, request):
+#         loc = '\\images\\stored\\test2.jpg'
+#         BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#         MEDIA_ROOT =os.path.join(BASE_DIR,'SchoolManagement')
+#         uri  = request.POST.__getitem__('image')
+#         with url_request.urlopen(uri) as resp:
+#             with open((str(MEDIA_ROOT)+'\\images\\image.jpg'), 'wb') as f:
+#                 f.write(resp.file.read())
+#         img = cv2.imread((str(MEDIA_ROOT)+'\\images\\image.jpg'),1)
 
 
-        print(MEDIA_ROOT,loc)
-        loc=(str(MEDIA_ROOT)+loc)
-        print(loc)
+#         print(MEDIA_ROOT,loc)
+#         loc=(str(MEDIA_ROOT)+loc)
+#         print(loc)
 
-        print("/images/stored/test.jpg")
-        face_1_image = face_recognition.load_image_file(loc)
-        face_1_face_encoding = face_recognition.face_encodings(face_1_image)[0]
+#         print("/images/stored/test.jpg")
+#         face_1_image = face_recognition.load_image_file(loc)
+#         face_1_face_encoding = face_recognition.face_encodings(face_1_image)[0]
 
 
-        small_frame = cv2.resize(img, (0, 0), fx=0.25, fy=0.25)
-        rgb_small_frame = small_frame[:, :, ::-1]
+#         small_frame = cv2.resize(img, (0, 0), fx=0.25, fy=0.25)
+#         rgb_small_frame = small_frame[:, :, ::-1]
 
-        face_locations = face_recognition.face_locations(rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+#         face_locations = face_recognition.face_locations(rgb_small_frame)
+#         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
-        check=face_recognition.compare_faces(face_1_face_encoding, face_encodings)
+#         check=face_recognition.compare_faces(face_1_face_encoding, face_encodings)
 
-        print(check)
-        if check[0]:
-                return True
+#         print(check)
+#         if check[0]:
+#                 return True
         
-        else :
-                return False
-    def get(self, request):
-        return render(request ,'faceid.html')
+#         else :
+#                 return False
+#     def get(self, request):
+#         return render(request ,'faceid.html')
 
 @login_required
 class profile(APIView):
@@ -74,19 +74,24 @@ class profile(APIView):
 
 def login(request):
     if request.method == 'POST':
-        username = form.cleaned_data.get('username')
-        raw_password = form.cleaned_data.get('password')
-        user = authenticate(username=username, password=raw_password)
-        if user is not None:
-            return profile(request)
+        form = AuthenticationForm(request = request, data = request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=raw_password)
+            if user is not None:
+                 return profile(request)
             
-        if user is not None: 
-            profile(request, user)
-            return HttpResponseRedirect(reverse('home'))
+            if user is not None: 
+                profile(request, user)
+
+            else: 
+                return HttpResponse('<p> User name or password is incorrect. Please try again.</p>')
         else: 
             return HttpResponse('<p> User name or password is incorrect. Please try again.</p>')
     else:
-        return render(request,'login.html') 
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
 
 def register(request):
     if request.method == 'POST':
